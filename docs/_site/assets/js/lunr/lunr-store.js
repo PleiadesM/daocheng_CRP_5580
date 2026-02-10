4144d50a5f517a68e05e151e522501ab8b4c9afa
Daocheng Lin <daocheng@iastate.edu>
2026-02-09 23:16:44 -0600

    Update the Jekyll for my localhost environment

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Assignments",
+        "excerpt":"Major graded assignments.  ","categories": [],
+        "tags": [],
+        "url": "/assignments/",
+        "teaser": null
+      },{
+        "title": "Exercises",
+        "excerpt":"Exercises completed throughout the course.  ","categories": [],
+        "tags": [],
+        "url": "/exercises/",
+        "teaser": null
+      },{
+        "title": "Final Project",
+        "excerpt":"Final project documentation and deliverables.  ","categories": [],
+        "tags": [],
+        "url": "/final-project/",
+        "teaser": null
+      },{
+        "title": "Tasks",
+        "excerpt":"This section contains task-based assignments and embedded outputs.  ","categories": [],
+        "tags": [],
+        "url": "/tasks/",
+        "teaser": null
+      },]
