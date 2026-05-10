c96df4e006e69164cce3da4940b368d5677cfc96
Daocheng Lin <daocheng@iastate.edu>
2026-05-10 15:56:53 -0500

    Activate the final project folder

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Assignments",
         "excerpt":"Major graded assignments.  ","categories": [],
         "tags": [],
-        "url": "/assignments/",
+        "url": "/daocheng_CRP_5580/assignments/",
         "teaser": null
       },{
         "title": "Exercises",
         "excerpt":"Exercises completed throughout the course.  ","categories": [],
         "tags": [],
-        "url": "/exercises/",
+        "url": "/daocheng_CRP_5580/exercises/",
         "teaser": null
       },{
         "title": "Final Project",
         "excerpt":"Final project documentation and deliverables.  ","categories": [],
         "tags": [],
-        "url": "/final-project/",
+        "url": "/daocheng_CRP_5580/final-project/",
         "teaser": null
       },{
         "title": "Tasks",
         "excerpt":"This section contains task-based assignments and embedded outputs.  ","categories": [],
         "tags": [],
-        "url": "/tasks/",
+        "url": "/daocheng_CRP_5580/tasks/",
         "teaser": null
       },]
